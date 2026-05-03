383712ec37256de36dd82d6007c7d5452b232234
hakimcolor <hakimcolor777@gmail.com>
2026-05-03 16:06:32 +0600

    object in function method

--- src/function.ts
+++ src/function.ts
@@ -1,23 +1,29 @@
-// Function 
-// arrow function ,normal Functon 
+// Function
+// arrow function ,normal Functon
 
-// use normal function and assign it to a variable 
-function func(num1 :number, num2:number):number {
+// use normal function and assign it to a variable
+function func(num1: number, num2: number): number {
   return num1 + num2;
-
 }
-  
-// arrow function 
+
+// arrow function
 
 const arr = (n: number, n2: number): number => {
   return n + n2;
-
-}
-
+};
 
 //object in function method;
 
+const poorUser = {
+  name: 'hakim',
+  balance: 0,
+  addBalance(value: number): number {
+    return this.balance + value;
+  },
+};
+poorUser.addBalance(10000);
 
-const pooruser = {
-  
-}
+const arrtyr: number[] = [1, 2, 3, 4, 5];
+
+const sqrArry = arrtyr.map((num: number): number => num * num);
+console.log(sqrArry);
