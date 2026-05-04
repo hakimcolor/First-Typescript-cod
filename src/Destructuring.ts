2e8a5569a268c79becb8c6a244770350c39253d8
hakimcolor <hakimcolor777@gmail.com>
2026-05-05 03:04:41 +0600

    destructuring alis in typescript

--- src/Destructuring.ts
+++ src/Destructuring.ts
@@ -13,8 +13,12 @@ const user = {
 // in js
 // const mNames = user.name.mName;
 //in ts
-const { name: { mName: M } } = user;
+const { name: { mName: M } } = user;// in the cod  the mName I replace wiht M  , now we can consol M ,, 
+
+// for changing the name of the varible we can do like this in ts  
+
 // console.log(M);
+
 // when we want to change the name of the variable we can do like this . in ts ok .....
 const friends = ['karim', 'Rahim', 'Atul '];
 // in js
