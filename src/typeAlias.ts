5cf4f42262da961c887c49f91a2d98e62d20f735
hakimcolor <hakimcolor777@gmail.com>
2026-05-06 23:04:33 +0600

    alias

--- src/typeAlias.ts
+++ src/typeAlias.ts
@@ -21,3 +21,9 @@ const user1: user = {
   city: 'sdlkfsd',
 };
 
+
+// function type alias 
+type addFunc = (num1: number, num2: number) => number;
+const add: addFunc = (num1, num2) => num1 + num2;
+
+// console.log(add(10,20));
