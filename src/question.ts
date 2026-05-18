6dc1340497f51122aee36e8ddabf32cb0a2e56af
hakimcolor <hakimcolor777@gmail.com>
2026-05-18 06:35:54 +0600

    optional chaining operator

--- src/question.ts
+++ src/question.ts
@@ -1,32 +1,39 @@
-// ?: ternary opearator   :decision making              
+// ?: ternary opearator   :decision making
 // ?? : nullish coalescing operator
 // ?. optinal chaining oprator
 
-
-
 const mage = (age: number) => {
   if (age >= 21) {
-  console.log('your are eligible');
-  
+    console.log('your are eligible');
   } else {
     console.log('your are not eligible');
-    
-}
-}
+  }
+};
 // mage(21)
 
 //ternary operator
 const isEligible = (age: number) => {
   const result = age >= 21 ? 'eligible' : 'not eligible';
   console.log(result);
-  
-}
+};
 // isEligible(18)
-
 
 // nullish coalescing operator
 const userThem = undefined;
 const theme = userThem ?? 'light ';
-console.log(theme);
+// console.log(theme);
 
-
+//optional chaining operator
+const userss: {
+  address: {
+    City: string;
+    town: string;
+    postalcode?: number;
+  };
+} = {
+  address: {
+    City: 'dhaka',
+    town: 'banani',
+  },
+};
+console.log(userss?.address?.postalcode ?? 'enteryour postcode');
