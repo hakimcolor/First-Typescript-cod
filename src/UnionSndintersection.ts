1f90789bde8a75583d374b9850afbeb125a397b1
hakimcolor <hakimcolor777@gmail.com>
2026-05-21 21:08:51 +0600

    my commit for commetn

--- src/UnionSndintersection.ts
+++ src/UnionSndintersection.ts
@@ -1,12 +1,12 @@
 type UserRole = 'admin' | 'editor';
 const getDashboard = (role: UserRole) => {
-  if (role === 'admin') {
-    return 'admin dashbord';
-  } else if (role === 'editor') {
-    return 'editor dashboard';
-  } else {
-    return 'no dashobord ';
-  }
+  // if (role === 'admin') {
+  //   return 'admin dashbord';
+  // } else if (role === 'editor') {
+  //   return 'editor dashboard';
+  // } else {
+  //   return 'no dashobord ';
+  // }
 };
 
 
